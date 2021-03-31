e7359bce585bb1e70816337098611d5ccfb91e8f
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-01 01:53:56 +0300

    Add basic logic and UI

--- App.js
+++ App.js
@@ -9,48 +9,101 @@ import {
   StatusBar,
   Image,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import { AddTaskComponent } from "./app/screens/addTask/AddTaskComponent";
 
 export default function App() {
-  // const [logoDisplay, setLogoDisplay] = useState(false);
-  // const handleMainScreenDisplay = function () {
-  //   setLogoDisplay(!true);
-  // };
-
+  const [welcomeScreen, setWelcomeScreenDisplay] = useState(true);
+  const handleWelcomeScreenDisplay = function () {
+    setWelcomeScreenDisplay(false);
+  };
+  setTimeout(handleWelcomeScreenDisplay, 3000);
   return (
-    // <WelcomeScreen />
-    <SafeAreaView style={styles.container}>
-      {/* <TouchableOpacity onPress={handleMainScreenDisplay}> */}
-      {/* {logoDisplay ? ( */}
-      <View style={styles.logo}>
-        <Image
-          style={styles.logoImage}
-          source={require("./app/assets/logo.png")}
-          // resizeMode="stretch"
-        />
-      </View>
-      {/* // ) : ( */}
-      <View>
-        {/* <Text>Planner</Text> */}
-        <AddTaskComponent />
-      </View>
-      {/* )} */}
-      {/* </TouchableOpacity> */}
-    </SafeAreaView>
+    <>
+      {welcomeScreen ? (
+        <WelcomeScreen />
+      ) : (
+        <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            <Image
+              style={styles.logoImage}
+              source={require("./app/assets/logo.png")}
+            />
+          </View>
+          <View style={styles.body}>
+            <View style={styles.tasksDisplay}>
+              <View style={styles.buttonsPanel}>
+                <View style={styles.buttonWrapper}>
+                  <Button
+                    style={styles.button}
+                    title="Today"
+                    onPress={() => Alert.alert("Simple Button pressed")}
+                  />
+                </View>
+                <View style={styles.buttonWrapper}>
+                  <Button
+                    style={styles.button}
+                    title="Week"
+                    onPress={() => Alert.alert("Simple Button pressed")}
+                  />
+                </View>
+                <View style={styles.buttonWrapper}>
+                  <Button
+                    style={styles.button}
+                    title="Month"
+                    onPress={() => Alert.alert("Simple Button pressed")}
+                  />
+                </View>
+              </View>
+            </View>
+            <View style={styles.addTaskComponentWrapper}>
+              <AddTaskComponent />
+            </View>
+          </View>
+        </SafeAreaView>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
+    backgroundColor: "#e9eae8",
+    // backgroundColor: "red",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: "center",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
   },
-  logoImage: {
-    resizeMode: "contain",
+  header: {
+    backgroundColor: "#dc953b",
+    // backgroundColor: "orange",
   },
+  body: {
+    flex: 1,
+    // backgroundColor: "yellow",
+    width: "100%",
+  },
+  tasksDisplay: {
+    flex: 1,
+    // backgroundColor: "greenyellow",
+  },
+  buttonsPanel: {
+    alignSelf: "stretch",
+    width: "100%",
+    flexDirection: "row",
+  },
+  buttonWrapper: {
+    flex: 1,
+  },
+  button: {
+    alignSelf: "stretch",
+  },
+  addTaskComponentWrapper: {
+    alignSelf: "stretch",
+    alignContent: "space-between",
+  },
+  logoImage: {},
 });
--- app/screens/addTask/AddTaskComponent.js
+++ app/screens/addTask/AddTaskComponent.js
@@ -6,6 +6,7 @@ import {
   View,
   Text,
   Button,
+  Alert,
 } from "react-native";
 
 export function AddTaskComponent() {
@@ -18,36 +19,52 @@ export function AddTaskComponent() {
   const [number, onChangeNumber] = React.useState(null);
 
   return (
-    <View>
+    <View style={styles.addTaskComponent}>
       <View style={styles.switchContainer}>
-        <Text>Important</Text>
-        <Switch
-          trackColor={{ false: "#767577", true: "#81b0ff" }}
-          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-          style={styles.switch}
-        />
+        <View style={styles.switchTextWrappew}>
+          <Text>Important</Text>
+        </View>
+        <View>
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+            style={styles.switch}
+          />
+        </View>
       </View>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="date placeholder"
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.textInput}
-        onChangeText={onChangeText}
-        value={text}
-        multiline
-        numberOfLines={4}
-      />
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert("Simple Button pressed")}
-      />
+      <View style={styles.textIputContainer}>
+        <View style={styles.inputsWrapper}>
+          <View style={styles.dateTextInput}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={onChangeNumber}
+              value={number}
+              placeholder="Enter date MM.DD.YYYY"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.taskTextInput}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={onChangeText}
+              value={text}
+              multiline
+              numberOfLines={4}
+            />
+          </View>
+        </View>
+        <View style={styles.addButtonWrapper}>
+          <Button
+            containerViewStyle={{ width: "100%", marginLeft: 0 }}
+            style={styles.addButton}
+            title="Add"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -55,16 +72,53 @@ export function AddTaskComponent() {
 export default AddTaskComponent;
 
 const styles = StyleSheet.create({
-  textInput: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-  },
-  switch: {
-    transform: [{ scaleX: 1.8 }, { scaleY: 1.8 }],
+  addTaskComponent: {
+    flex: 1,
+    flexDirection: "column",
+    // backgroundColor: "dodgerblue",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   switchContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
+    backgroundColor: "gray",
+    // backgroundColor: "green",
+  },
+  switch: {
+    // transform: [{ scaleX: 1.8 }, { scaleY: 1.8 }],
+  },
+  switchTextWrappew: {
+    justifyContent: "center",
+    alignContent: "center",
+    marginHorizontal: 7,
+  },
+  textIputContainer: {
+    flex: 1,
+    backgroundColor: "#e9eae8",
+    // backgroundColor: "purple",
+    justifyContent: "center",
+    padding: 15,
+  },
+  textInput: {
+    borderWidth: 1,
+    marginBottom: 3,
+    paddingHorizontal: 7,
+  },
+  dateTextInput: {
+    marginBottom: 13,
+  },
+  taskTextInput: {
+    marginBottom: 13,
+  },
+  addButtonWrapper: {
+    flexDirection: "column",
+    alignItems: "stretch",
+    flex: 1,
+  },
+  addButton: {
+    flex: 1,
   },
 });
